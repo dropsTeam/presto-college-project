3593da7c12e303c3bc1c4d3499694d581869a183
Jashanpreet Singh <51172269+this-is-jassa@users.noreply.github.com>
2020-08-17 00:27:48 -0400

    changes

--- dbOperations.py
+++ dbOperations.py
@@ -9,13 +9,19 @@ class dbOperations:
         connection = MongoClient('localhost:27017')
         self.__db = connection['teamdatabase']
         self.__userCollection = self.__db['users']
-    
+
+
+    def isUserExist(self, passportNumber, isAdmin):
+        return self.__userCollection.count({"passportNumber": passportNumber, "isAdmin": isAdmin}, { limit: 1 }) != 0
+
 
     def findUser(self, passportNumber, isAdmin):
         return self.__userCollection.find_one({"passportNumber": passportNumber, "isAdmin": isAdmin})
     
     
+    
     def loginAdmin(self, passportNumber, password): 
+        
         user = self.findUser(passportNumber, True)
         
         if bcrypt.checkpw(password, user["password"]):
@@ -24,17 +30,22 @@ class dbOperations:
             return False
     
 
-    def registerAdmin(self, userName, password, address, passportNumber, country):
+    
+    def registerAdmin(self, userName, password, address, passportNumber, country, email):
          
         if(self.__userCollection.count({"passportNumber": passportNumber, "isAdmin": True}, { limit: 1 }) != 0): 
             raise Exception("User Already exist, try with diffent passport number.")
         else:
             hashed = bcrypt.kdf( password= password, salt='salt', desired_key_bytes=32, rounds=100)
-            self.__userCollection.insert_one({"userName": userName, "password": hashed, "isAdmin": True, "balance": 0, "address": address, "country": country, "passportNumber": passportNumber})
+            self.__userCollection.insert_one({"userName": userName, "password": hashed, "isAdmin": True, "balance": 0, "address": address, "country": country, "passportNumber": passportNumber, "email": email})
     
-    def addUser(self, userName, address, balance, country, passportNumber):
+    
+    def addUser(self, userName, address, balance, country, passportNumber, email):
         
         if(self.__userCollection.count({"passportNumber": passportNumber, "isAdmin": False}, { limit: 1 }) != 0): 
             raise Exception("User Already exist, try with different passport number.")
         else:
-            self.__userCollection.insert_one({"userName": userName, "isAdmin": False, "balance": balance, "address": address, "country": country, "passportNumber": passportNumber})
+            self.__userCollection.insert_one({"userName": userName, "isAdmin": False, "balance": balance, "address": address, "country": country, "passportNumber": passportNumber, "email": email})
+
+
+
