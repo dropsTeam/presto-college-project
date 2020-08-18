4407552c676cc4126ea9c278ab93fd2275840d49
Jashanpreet Singh <51172269+this-is-jassa@users.noreply.github.com>
2020-08-17 22:43:35 -0400

    major bugs fixed

--- dbOperations.py
+++ dbOperations.py
@@ -1,23 +1,24 @@
 from pymongo import MongoClient
-import bcrypt
+
 
 class dbOperations:
     
     def __init__(self):
-        super().__init__()
 
         connection = MongoClient('localhost:27017')
         self.__db = connection['teamdatabase']
         self.__userCollection = self.__db['users']
 
         if( self.__userCollection.find({"isAdmin": True}).count() == 0 ):
-            self.registerAdmin("Sagara Samarawickrama","12345", "Address not added", "P00000", "Canada", "abc@gmail.com", 123456789 )
+            self.registerAdmin("Sagara Samarawickrama","12345", "Address not added", "P00000", "abc@gmail.com", 123456789 )
 
 
 
 
     def isUserExist(self, passportNumber, isAdmin):
-        return self.__userCollection.count({"passportNumber": passportNumber, "isAdmin": isAdmin}, { limit: 1 }) != 0
+        print(self.__userCollection.find({"passportNumber": passportNumber, "isAdmin": isAdmin}, { "limit": 1 }))
+        return False
+        
 
 
     def findUser(self, passportNumber, isAdmin):
@@ -29,7 +30,7 @@ class dbOperations:
         
         if (self.isUserExist(passportNumber, True)):
             user = self.findUser(passportNumber, True)
-            if bcrypt.checkpw(password, user["password"]):
+            if user["password"] == password:
                 return True
             else:
                 return False
@@ -45,8 +46,8 @@ class dbOperations:
         elif( self.isUserExist(passportNumber, True) ): 
             raise Exception("User Already exist, try with diffent passport number.")
         else:
-            hashed = bcrypt.kdf( password= password, salt='salt', desired_key_bytes=32, rounds=100)
-            self.__userCollection.insert_one({"userName": userName, "password": hashed, "isAdmin": True, "balance": 0, "address": address, "passportNumber": passportNumber, "email": email, "contact": contact})
+            
+            self.__userCollection.insert_one({"userName": userName, "password": password, "isAdmin": True, "balance": 0, "address": address, "passportNumber": passportNumber, "email": email, "contact": contact})
     
     
     def addUser(self, userName, address, balance, passportNumber, email, contact):
